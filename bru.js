e63602096a6f4e88390d80ee572522d135ab928a
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2023-07-22 16:38:19 +0530

    first commit

---
+++ bru.js
@@ -0,0 +1,8 @@
+function on(){
+    var x=document.getElementById('listItems1');
+    if (x.style.display == "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display= "none";
+    }
+}
